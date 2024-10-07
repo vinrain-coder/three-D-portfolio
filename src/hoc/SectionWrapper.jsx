061f4125c956e6ff6b5f7ede3c0646b880aa7c95
vinrain-coder <vincentombogo57@gmail.com>
2024-10-07 11:52:07 +0000

    modified about section

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -8,7 +8,7 @@ const SectionWrapper = (Component, idName) =>
       <motion.section
         variants={staggerContainer}
         initial="hidden"
-        whileInView="show"  // Corrected the show reference
+        whileInView="show"  
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
