10e621b39bde81595777de4d491e92d0b9d6d9df
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-10-09 23:07:01 +0000

    removed 3d

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+// import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -26,7 +26,7 @@ const Hero = () => {
       </div>
 
       {/* 3D Canvas */}
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
       {/* Scroll indicator */}
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center mt-4">
